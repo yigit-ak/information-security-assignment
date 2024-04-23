fb6fd7a94dba46245d1015db89485a4ee51bf200
Yigit M. AK <ak.yigitmesut@gmail.com>
2024-04-23 13:34:39 +0300

    project template

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class Main {
+    public static void main(String[] args) {
+        // key pair to be created using RSA
+        PublicKey publicKeyA;
+        PrivateKey privateKeyA;
+
+        // key pairs to be created using ecliptic curve
+        PublicKey publicKeyB;
+        PrivateKey privateKeyB;
+        PublicKey publicKeyC;
+        PrivateKey privateKeyC;
+
+        // TODO generate key pairs
+
+        // TODO generate digital signature for the image
+
+        // TODO AES encryption
+
+        // TODO create message authentication code for text message
+
+    }
+}
